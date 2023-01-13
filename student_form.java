309b1cf96e881a5662be629d1a5c983d8f47f5df
Namrata Bhosale <104768013+namratabhosale20@users.noreply.github.com>
2023-01-13 19:49:12 +0530

    Add files via upload

---
+++ student_form.java
@@ -0,0 +1,50 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+class student_form extends Frame implements ActionListener
+{
+    // Label lb1,lb2,lb3,lb4,lb5,lb6,lb7,lb8;
+    // TextField t1,t2,t3,t4,t5,t6,t7;
+    // Button b1,b2,b3;
+
+    // student_form()
+    // {
+    //   setSize(1200,800);
+    //   setVisible(true);
+    //   setLayout(null);
+    //   lb1=new Label("Numeric oprations on 3 numbers");
+    //   lb2=new Label("Enter first no.");
+    //   lb3=new Label("Enter second no.");
+    //   lb4=new Label("Enter third no.");
+    //   lb5=new Label("result");
+    //   t1=new TextField();
+    //   t2=new TextField();
+    //   t3=new TextField();
+    //   t4=new TextField();
+    //   b1=new Button("largest");
+    //   b2=new Button("smallest");
+    //   b3=new Button("result");
+    //   b1.addActionListener(this);
+    //   b2.addActionListener(this);
+    //   b3.addActionListener(this);
+    //   add(lb1);
+    //   add(lb2);
+    //   add(t1);
+    //   add(lb3);
+    //   add(t2);
+    //   add(lb4);
+    //   add(t3);
+    //   add(b1);
+    //   add(b2);
+    //   add(b3);
+
+    //   lb1.setBounds(400,100, 450, 30);
+
+
+    // }
+    // public static void main(String args[]) 
+    // {
+    // student_form obj = new student_form();
+    // }
+
+}
