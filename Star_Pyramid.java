da78d885c1bd900194832914d5501fb74527c740
Namrata Bhosale <104768013+namratabhosale20@users.noreply.github.com>
2023-01-18 10:05:38 +0530

    Add files via upload

---
+++ Star_Pyramid.java
@@ -0,0 +1,17 @@
+public class Star_Pyramid {
+    public static void main(String args[])
+    {
+        for(int i=1;i<=4;i++)
+        {
+            for(int k=4;k>=i;k--)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+    }
+}
